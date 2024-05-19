feea0b1cf905a490fdfcc4c91a6fdb737c7182a2
Olivel Cantoran <ocg101010@gmail.com>
2024-05-18 19:14:08 -0600

    Añadiendo parametros para actualizacion de archivos

--- Scripting_Bash/Buscador.sh
+++ Scripting_Bash/Buscador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # htbmachines.sh
 
-#Colours
+# COLOURS
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
 redColour="\e[0;31m\033[1m"
@@ -11,36 +11,73 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
+# VARIABLES GLOBALES
+mainUrl="https://htbmachines.github.io/bundle.js"
+
+
+# FUNCIONES
 function ctrl_c () {
   echo -e "$\n\n${redColour}[+]Saliendo...\n${endColour}"
-  exit 1
+  tput cnorm && exit 1
 }
 function helpPanel () {
-  echo -e "[+] Uso:"
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso:${endColour}"
+  echo -e "\t${purpleColour}u)${endColour}${grayColour} Descargar o actualizar archivos necesarios${endColour}"
+  echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por un nombre de maquina${endColour}"
+  echo -e "\t${purpleColour}h)${endColour}${grayColour} Motrar este panel de ayuda${endColour}\n"
 }
 function searchMachine (){
   machineName="$1"
   echo ${machineName}
 }
+function updateFiles (){
+  if [ ! -f bundle.js ]; then
+      tput civis
+      echo -e "${yellowColour}[+]${endColour}${grayColour} Descargando archivos necesarios...${endColour}\n"
+      curl -s ${mainUrl} > bundle.js
+      # js-beautify bundle.js | sponge bundle.js
+      echo -e "${yellowColour}[+]${endColour}${grayColour} Todos los archivos han sido descargados${endColour}\n"
+      tput cnorm
+  else
+      tput civis
+      echo -e "${yellowColour}[+]${endColour}${grayColour}Comprobando si hay actualizaciones pendientes${endColour}\n"
+      curl -s ${mainUrl} > bundle_temp.js
+      # js-beautify bundle_temp.js | sponge bundle_temp.js
+      md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
+      md5_original_value=$(md5sum bundle.js | awk '{print $1}')
+      if [ "$md5_temp_value" == "$md5_original_value" ]; then
+          echo -e "${yellowColour}[+]${endColour}${grayColour} No se han detectado actualizaciones, lo tienes todo al día ;)${endColour}\n"
+          rm -r bundle_temp.js
+      else
+          echo -e "${yellowColour}[+]${endColour}${grayColour} Se han encontrado actualizaciones disponibles${endColour}\n"
+          sleep 1
+          rm -rf bundle.js && mv bundle_temp.js bundle.js
+          echo -e "${yellowColour}[+]${endColour}${grayColour}Los archivos han sido actualizados${endColour}\n"
+      fi
+      tput cnorm
+  fi
+}
 
-# Ctrl+C
+# CRTL+C
 trap ctrl_c INT
-sleep 10
 
 # INDICADORES
 declare -i parameter_counter=0
 
 
 # MENU
-while getopts "m:h" arg; do
+while getopts "m:uh" arg; do
   case $arg in
     m) machineName=$OPTARG; let parameter_counter+=1;;
-    h)  
+    u) let parameter_counter+=2;;
+    h) ;;
   esac
 done
 
 if [ ${parameter_counter} -eq 1 ]; then
-  searchMachine ${machineName}
+    searchMachine ${machineName}
+elif [ ${parameter_counter} -eq 2 ]; then
+    updateFiles
 else
-  helpPanel
+    helpPanel
 fi
