498d477174eefc2fb1f1621441a3c054a828f92f
Olivel Cantoran <ocg101010@gmail.com>
2024-05-19 23:27:34 -0600

    Finalizando script con la incorporacion de busqueda por skills

--- Scripting_Bash/Buscador.sh
+++ Scripting_Bash/Buscador.sh
@@ -27,6 +27,7 @@ function helpPanel () {
   echo -e "\t${purpleColour}i)${endColour}${grayColour} Buscar por dirección IP${endColour}"
   echo -e "\t${purpleColour}d)${endColour}${grayColour} Buscar por la dificultad de una máquina${endColour}"
   echo -e "\t${purpleColour}o)${endColour}${grayColour} Buscar por el sistema operativo${endColour}"
+  echo -e "\t${purpleColour}s)${endColour}${grayColour} Buscar por skill${endColour}"
   echo -e "\t${purpleColour}y)${endColour}${grayColour} Obtener link de la resolución de la máquina en YouTube${endColour}"
   
   echo -e "\t${purpleColour}h)${endColour}${grayColour} Motrar este panel de ayuda${endColour}\n"
@@ -137,6 +138,19 @@ function getOSDifficultyMacbines () {
   fi
 }
 
+function getSkill (){
+  skill="$1"
+
+  check_skill="$(cat bundle.js  | grep 'skills:' -C 6 | grep -i "Snmp enumeration" -B 7 | grep 'name: ' | awk 'NF {print $NF}' | tr -d '"' | tr -d "," | column)"
+
+  if [ "${check_skill}" ]; then
+      echo -e "${yellowColour}[+]${endColour}${grayColour} A continuación se representan las maquinas donde se ve toca la skill ${endColour}${blueColour}${skill}${endColour}${grayColour}:${endColour}\n"
+      cat bundle.js  | grep 'skills:' -C 6 | grep -i "${skill}" -B 7 | grep 'name: ' | awk 'NF {print $NF}' | tr -d '"' | tr -d "," | column
+  else
+      echo -e "\n${redColour}[!] No se ha encontrado ninguna máquina con la skill indicada ${endColour}\n"
+  fi
+}
+
 # CRTL+C
 trap ctrl_c INT
 
@@ -151,7 +165,7 @@ declare -i chivato_os=0
 
 
 # MENU
-while getopts "m:ui:y:d:o:h" arg; do
+while getopts "m:ui:y:d:o:s:h" arg; do
   case $arg in
     m) machineName="$OPTARG"; let parameter_counter+=1;;
     u) let parameter_counter+=2;;
@@ -160,6 +174,7 @@ while getopts "m:ui:y:d:o:h" arg; do
     y) machineName="$OPTARG"; parameter_counter+=4;;
     d) difficulty="$OPTARG"; chivato_difficulty=1; parameter_counter+=5;;
     o) os="$OPTARG"; chivato_os=1; parameter_counter+=6;;
+    s) skill="$OPTARG"; parameter_counter+=7;;
   esac
 done
 
@@ -175,6 +190,8 @@ elif [ ${parameter_counter} -eq 5 ]; then
     getMachinesDifficulty ${difficulty}
 elif [ ${parameter_counter} -eq 6 ]; then
     getOSMachines $os
+elif [ ${parameter_counter} -eq 7 ]; then
+    getSkill "$skill"
 elif [ ${chivato_difficulty} -eq 1 ] && [ ${chivato_os} -eq 1 ]; then
     getOSDifficultyMacbines $difficulty $os 
 else   
