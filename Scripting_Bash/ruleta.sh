a310d046763219f38210c71d4ce343b0a237e61f
Olivel Cantoran <ocg101010@gmail.com>
2024-05-28 22:08:48 -0600

    Agregando la operatoria a efectuar eb cuanto a beneficio respecta

--- Scripting_Bash/ruleta.sh
+++ Scripting_Bash/ruleta.sh
@@ -30,22 +30,40 @@ function martingala (){
 
   echo -e "\n${yellowColour}[+]${endColour}${grayColour} Vamos a iniciar con una cantidad de${endColour}${yellowColour} ${initial_bet}€${endColour}${grayColour} a${endColour}${yellowColour} ${par_impar}${endColour}" 
 
+  backup_bet=$initial_bet
   tput civis  # Ocultar el cursor
   while true; do
+    money=$(($money - $initial_bet))
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Acabas de apostar${endColour} ${yellowColour}${initial_bet}€${endColour}${grayColour} y tienes${endColour}${yellowColour} ${money}€${endColour}"
     random_number="$(($RANDOM % 37))"
-    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Ha salido el número${endColour}${blueColour} ${random_number}${endColour}"
+    echo -e "${yellowColour}[+]${endColour}${grayColour} Ha salido el número${endColour}${yellowColour} ${random_number}${endColour}"
+    if [ ! "$money" -le 0 ]; then
+      if [ "$par_impar" == "par" ]; then
+        if [ "$(($random_number % 2))" -eq 0 ]; then
+          if [ "$random_number" -eq 0  ]; then
+            echo -e "${redColour}[+] Ha salido el 0, por lo tanto perdemos${endColour}"
+            initial_bet=$(($initial_bet * 2))
+            echo -e "${yellowColour}[+]${endColour}${grayColour}Ahora mismo te quedas en ${endColour}${yellowColour}${money}€${endColour}"
 
-    if [ "$(($random_number % 2))" -eq 0 ]; then
-      if [ "$random_number" -eq 0  ]; then
-        echo -e "[+] Ha salido el 0, por lo tanto perdemos"
-      else
-        echo -e "[+] El número que ha salido es par"
+          else
+            echo -e "${yellowColour}[+]${endColour}${greenColour} El número que ha salido es par ¡ganas!${endColour}"
+            reward=$(($initial_bet * 2))
+            echo -e "${yellowColour}[+]${endColour}${grayColour} Ganas un total de ${endColour}${yellowColour}${reward}€${endColour}"
+            money=$(($money + $reward))
+            echo -e "${yellowColour}[+]${endColour}${grayColour} Tienes${endColour}${yellowColour} ${money}${endColour}"
+            initial_bet=$backup_bet
+          fi
+        else
+          echo -e "${yellowColour}[+]${endColour}${redColour} El número que ha salido es impar y ¡pierdes!${endColour}"
+          initial_bet=$(($initial_bet * 2))
+          echo -e "${yellowColour}[+]${endColour}${grayColour}Ahora mismo te quedas en ${endColour}${yellowColour}${money}€${endColour}"
+        fi
       fi
     else
-       echo -e "[+] El número que ha salido es impar"
-    fi
-
-    sleep 0.4
+      # Nos quedamos sin dinero
+      echo -e "\n${redColour}[!] Te has quedado sin dinero${endColour}" 
+      tput cnorm; exit 0
+    fi  
   done
 
   tput cnorm # Recuperamos el cursor
