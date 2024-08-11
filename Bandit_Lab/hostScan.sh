fc726a8d84530124113f0c3b518cc124559ecf0c
Olivel Cantoran <ocg101010@gmail.com>
2024-08-10 18:37:42 -0600

    Arreglando errores de scripts

--- Bandit_Lab/hostScan.sh
+++ Bandit_Lab/hostScan.sh
@@ -2,8 +2,8 @@
 
 function ctrl_c(){
   echo -e "[+]\n\n Saliendo....\n"
-  tput cnorm 
-  pkill -P $$
+  tput cnorm && exit 1
+#  pkill -P $$
   exit 1
 }
 
@@ -14,8 +14,7 @@ tput civis # Ocultar el cursos
 
 for i in $(seq 1 254); do
   timeout 1 bash -c "ping -c 1 10.0.0.${i}" &>/dev/null && echo "[+] HOST 10.0.0.${i} ACTIVE" || echo "[+] HOST 10.0.0.${i} DESACTIVE" &
-done;
-wait
+done; wait
 
 # Recuperar el cursor
 tput cnorm
