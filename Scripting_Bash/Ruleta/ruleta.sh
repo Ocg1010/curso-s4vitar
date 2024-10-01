9ee4501af23432a50b2bda64f9d6f30309b52f30
Olivel Cantoran <ocg101010@gmail.com>
2024-10-01 00:48:58 -0600

    Agregando modulo comentando las trazas

--- Scripting_Bash/Ruleta/ruleta.sh
+++ Scripting_Bash/Ruleta/ruleta.sh
@@ -24,6 +24,7 @@ function helpPanel(){
   echo -e "\t${blueColour}-t)${endColour} ${grayColour}Técnica a utilizar${endColour} ${purpleColour}(${endColour}${yellowColour}martingala${endColour}${blueColour}/${endColour}${yellowColour}inverseLabrouchere${endColour}${purpleColour})${endColour}\n"
   exit 1
 }
+
 function martingala (){
   
   echo -e "\n${yellowColour}[+]${endColour}${grayColour} Dinero actual: ${endColour}${yellowColour}${money}€${endColour}"
@@ -31,25 +32,56 @@ function martingala (){
   echo -ne "${yellowColour}[+]${endColour} ${grayColour}¿A qué desea apostar continuamente (par/impar)? -> ${endColour}" && read par_impar
 
   echo -ne "\n${yellowColour}[+]${endColour} ${grayColour}Vamos a jugar con una cantidad inicial de${endColour}${yellowColour} ${initial_bet}€${endColour}${grayColour} a${endColour}${yellowColour} ${par_impar}${endColour}\n"
+  
+  backup_bet=${initial_bet}
+  play_counter=1
+  jugadas_malas="["
+
   tput civis
   while true; do
+    money=$((${money} - ${initial_bet}))
+    #echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Acabas de apostar${endColour} ${yellowColour}${initial_bet}€${endColour}${grayColour} y tienes${endColour}${yellowColour} ${money}€${endColour}"
     random_number=$((RANDOM % 37))
-    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Ha salido el número${endColour}${blueColour} ${random_number}${endColour}"
-    if [[ "$((random_number % 2))" -eq 0 ]]; then
-      if [[ ${random_number} -eq 0 ]]; then
-        echo -e "[+] Ha salido el 0, por lo tanto perdemos"
-      else
-      echo -e "[+] El número que ha salido es par"
+    #echo -e "${yellowColour}[+]${endColour}${grayColour} Ha salido el número${endColour}${yellowColour} ${random_number}${endColour}"
+
+    if [[ ! ${money} -le 0 ]]; then
+      if [[ ${par_impar} == "par" ]]; then
+        if [[ "$((random_number % 2))" -eq 0 ]]; then
+          if [[ ${random_number} -eq 0 ]]; then
+            #echo -e "${redColour}[!] Ha salido el 0, por lo tanto perdemos${endColour}"
+            initial_bet=$((${initial_bet} * 2))
+            jugadas_malas+="${random_number} "
+            #echo -e "${yellowColour}[+]${endColour}${grayColour} Ahora mismo te quedas en${endColour}${yellowColour} ${money}€${endColour}"
+          else
+            #echo -e "${yellowColour}[+]${endColour}${greenColour} El número que ha salido es par ¡Ganas!${endColour}"
+            reward=$((${initial_bet} * 2))
+            #echo -e "${yellowColour}[+]${endColour}${grayColour} Ganas un total de${endColour}${yellowColour} ${reward}€${endColour}"
+            money=$((${money} + ${reward}))
+            #echo -e "${yellowColour}[+]${endColour}${grayColour} Tienes${endColour}${yellowColour} ${money}€${endColour}"
+            initial_bet=$((${backup_bet}))
+            jugadas_malas=""
+          fi
+        else
+          #echo -e "${redColour}[+] El número que ha salido es impar ¡Pierdes!${endColour}"
+          initial_bet=$((${initial_bet} * 2))
+          jugadas_malas+="${random_number} "
+          #echo -e "${yellowColour}[+]${endColour}${grayColour} Ahora mismo te quedas en${endColour}${yellowColour} ${money}€${endColour}"
+        fi
       fi
+      
     else
-      echo -e "[+] El número que ha salido es impar"
+      # Nos quedamos sin dinero
+      echo -e "\n${redColour}[!] Te has quedado sin dinero cabrón${endColour}\n"
+      echo -e "${yellowColour}[+]${endColour}${grayColour} Han habido un total de${endColour}${yellowColour} ${play_counter}${endColour}${grayColour} jugadas${endColour}"
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} A continuación se van a representar las jugadas malas consecutivas que han salido${endColour}"
+      echo -e "${blueColour} ${jugadas_malas}${endColour}"
+      tput cnorm && exit 0
     fi
-    sleep 2
+    let play_counter+=1
   done
-
-
   tput cnorm
 }
+
 while getopts "m:t:h" arg; do
   case $arg in 
     m) money=$OPTARG;;
